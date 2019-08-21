bb83908b11969b40ff14a938b332b82a93cf98df
John Griepentrog <1390583+jgriepentrog@users.noreply.github.com>
2019-08-20 22:46:46 -0500

    Refactor out a few functions
    Former-commit-id: 3f9d5e6bd20105479490dd38d1f09eac8f181da6

--- index.js
+++ index.js
@@ -5,10 +5,92 @@ const fs = require('fs')
 // const test = process.argv.slice(2)
 // console.log(test)
 
+const waitForStackSetOperationsComplete = async (stackSetName, operationIds) => {
+  const cloudformation = new AWS.CloudFormation({ apiVersion: '2010-05-15' })
+  const opIds = new Set(operationIds)
+
+  while (opIds.size > 0) {
+    let stackInstanceOpIdsToReview = opIds.size
+    let nextToken = true
+
+    while (nextToken) {
+      const listStackSetOperationsParams = {
+        StackSetName: stackSetName
+      }
+      const { Summaries: summaries, NextToken } = await cloudformation.listStackSetOperations(listStackSetOperationsParams).promise()
+      nextToken = NextToken
+
+      for (const summary of summaries) {
+        const { OperationId: operationId, Status: status } = summary
+        if (opIds.has(operationId)) {
+          switch (status) {
+            case 'FAILED':
+            case 'STOPPED':
+            case 'STOPPING':
+              throw new Error('Stack update failed or was cancelled. Review stack set events to determine cause.')
+
+            case 'SUCCEEDED':
+              opIds.delete(operationId)
+              break
+
+            case 'RUNNING':
+              break
+
+            default:
+              throw new Error('Unrecognized StackSet Operation!')
+          }
+          stackInstanceOpIdsToReview--
+        }
+
+        if (stackInstanceOpIdsToReview <= 0) {
+          nextToken = false
+          break
+        }
+      }
+
+      await new Promise((resolve) => {
+        setTimeout(resolve, 5 * 1000)
+      })
+    }
+  }
+}
+
+const createOrUpdateStackSet = async (stackSetName, templateURL) => {
+  const cloudformation = new AWS.CloudFormation({ apiVersion: '2010-05-15' })
+  const stackSetParams = {
+    StackSetName: stackSetName,
+    Capabilities: ['CAPABILITY_NAMED_IAM'],
+    TemplateURL: templateURL
+  }
+  try {
+    const { OperationId } = await cloudformation.updateStackSet(stackSetParams).promise()
+    await waitForStackSetOperationsComplete(stackSetName, [OperationId])
+  } catch (err) {
+    if (err.code !== 'StackSetNotFoundException') {
+      throw err
+    }
+    await cloudformation.createStackSet(stackSetParams).promise()
+  }
+}
+
+const uploadTemplate = async (templatePath, s3Bucket, s3Key) => {
+  const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
+  const s3FullKey = s3Key ? `${s3Key}/${templatePath}` : templatePath
+  const templateReadStream = fs.createReadStream(templatePath)
+  const s3UploadParams = {
+    Bucket: s3Bucket,
+    Key: s3FullKey,
+    ServerSideEncryption: 'AES256',
+    Body: templateReadStream
+  }
+  const { Location: templateURL } = await s3.upload(s3UploadParams).promise()
+  return templateURL
+}
+
 const main = async () => {
   /* Get params from file - temp use hard code */
   const stackSetName = 'test-stacksets'
-  const templateName = 'template.yml'
+  const templatePath = 'template.yml'
   const s3Bucket = 'test-stacksets-us-east-1'
   const s3Key = ''
   const defaultRegion = 'us-east-1'
@@ -18,66 +100,15 @@ const main = async () => {
   }
 
   const cloudformation = new AWS.CloudFormation({ apiVersion: '2010-05-15' })
-  const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
   const sts = new AWS.STS({ apiVersion: '2011-06-15' })
 
   const { Account: defaultAccountId } = await sts.getCallerIdentity({}).promise()
   const targets = { [defaultAccountId]: { 'us-east-1': true, 'us-west-2': true } }
 
-  const s3FullKey = s3Key ? `${s3Key}/${templateName}` : templateName
-  const templateReadStream = fs.createReadStream(templateName)
-  const s3UploadParams = {
-    Bucket: s3Bucket,
-    Key: s3FullKey,
-    ServerSideEncryption: 'AES256',
-    Body: templateReadStream
-  }
-  const { Location: templateURL } = await s3.upload(s3UploadParams).promise()
+  const templateURL = uploadTemplate(templatePath, s3Bucket, s3Key)
 
-  const stackSetParams = {
-    StackSetName: stackSetName,
-    Capabilities: ['CAPABILITY_NAMED_IAM'],
-    TemplateURL: templateURL
-  }
-  try {
-    const { OperationId } = await cloudformation.updateStackSet(stackSetParams).promise()
-
-    let operationNotDone = true
-    do {
-      const describeStackSetOpParams = {
-        OperationId,
-        StackSetName: stackSetName
-      }
-
-      const { StackSetOperation: { Status } } = await cloudformation.describeStackSetOperation(describeStackSetOpParams).promise()
-
-      switch (Status) {
-        case 'FAILED':
-        case 'STOPPED':
-        case 'STOPPING':
-          throw new Error('Stack update failed or was cancelled. Review stack set events to determine cause.')
-
-        case 'SUCCEEDED':
-          operationNotDone = false
-          break
-
-        case 'RUNNING':
-          await new Promise((resolve) => {
-            setTimeout(resolve, 10 * 1000)
-          })
-          break
-
-        default:
-          throw new Error('Unrecognized StackSet Operation!')
-      }
-    } while (operationNotDone)
-  } catch (err) {
-    if (err.code !== 'StackSetNotFoundException') {
-      throw err
-    }
-
-    await cloudformation.createStackSet(stackSetParams).promise()
-  }
+  /* Update/create Stack Set */
+  await createOrUpdateStackSet(stackSetName, templateURL)
 
   /* Check and make stack set instance adjustments if needed */
   const listStackInstancesParams = {
@@ -107,8 +138,6 @@ const main = async () => {
 
   const createStackInstancePromises = []
 
-  console.log(targets[defaultAccountId])
-
   for (const account of Object.keys(targets)) {
     const createStackInstanceParams = {
       StackSetName: stackSetName,
@@ -121,64 +150,11 @@ const main = async () => {
     }
   }
 
-  const createStackInstanceOpIds = new Set((await Promise.all(createStackInstancePromises)).map((data) => {
+  const createStackInstanceOpIds = (await Promise.all(createStackInstancePromises)).map((data) => {
     return data.OperationId
-  }))
+  })
 
-  while (createStackInstanceOpIds.size > 0) {
-    console.log(createStackInstanceOpIds)
-    let stackInstanceOpIdsToReview = createStackInstanceOpIds.size
-    let nextToken = true
-
-    while (nextToken) {
-      const listStackSetOperationsParams = {
-        StackSetName: stackSetName
-      }
-      const { Summaries: summaries, NextToken } = await cloudformation.listStackSetOperations(listStackSetOperationsParams).promise()
-      nextToken = NextToken
-      console.log(nextToken)
-
-      for (const summary of summaries) {
-        const { OperationId: operationId, Status: status } = summary
-        console.log(operationId)
-        console.log(status)
-        if (createStackInstanceOpIds.has(operationId)) {
-          switch (status) {
-            case 'FAILED':
-            case 'STOPPED':
-            case 'STOPPING':
-              throw new Error('Stack update failed or was cancelled. Review stack set events to determine cause.')
-
-            case 'SUCCEEDED':
-              createStackInstanceOpIds.delete(operationId)
-              break
-
-            case 'RUNNING':
-              break
-
-            default:
-              throw new Error('Unrecognized StackSet Operation!')
-          }
-          stackInstanceOpIdsToReview--
-        }
-
-        console.log(stackInstanceOpIdsToReview)
-        if (stackInstanceOpIdsToReview <= 0) {
-          nextToken = false
-          break
-        }
-      }
-
-      /*
-      if (!nextToken && stackInstanceOpIdsToReview > 0) {
-        throw new Error('Outstanding operations still to review, but no next token available')
-      } */
-
-      await new Promise((resolve) => {
-        setTimeout(resolve, 10 * 1000)
-      })
-    }
-  }
+  await waitForStackSetOperationsComplete(stackSetName, createStackInstanceOpIds)
 
   /*
   const createStackInstanceOpIds = (Promise.all(createStackInstancePromises)).sort()
