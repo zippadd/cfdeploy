a0857896e6f761bf3a2cdde23f197c41aaea8256
John Griepentrog <1390583+jgriepentrog@users.noreply.github.com>
2019-09-29 00:12:09 -0500

    Enable option to provide file name. Add a few additional checks in the file load.

--- src/stackSet/getSettings.test.js
+++ src/stackSet/getSettings.test.js
@@ -3,15 +3,9 @@ const AWS = require('aws-sdk-mock')
 const AWS_SDK = require('aws-sdk')
 AWS.setSDKInstance(AWS_SDK)
 
-const getPsuedoRandBetween = (minNum, maxNum) => {
-  return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum)
-}
-
-/* AWS Account numbers are 12 digits so set a corresponding max and min */
-const AWS_ACCT_NUM_MAX = 999999999999
-const AWS_ACCT_NUM_MIN = /* 00000000000 */1
 const stackSetName = 'test-stacksets'
-const mockAWSAcctNum = getPsuedoRandBetween(AWS_ACCT_NUM_MIN, AWS_ACCT_NUM_MAX)
+const mockAWSAcctNum = 123456789012
+const mockAWSAcctNum2 = 111111111111
 
 AWS.mock('STS', 'getCallerIdentity', jest.fn((params, callback) => {
   return callback(null, { Account: mockAWSAcctNum })
@@ -33,5 +27,55 @@ describe('Get Settings', () => {
         }
       }
     })
+  })
+  test('Returns sample cfdeploy settings object read from file with a specified account id', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-nondefault.yml')).resolves.toEqual({
+      stackSetName,
+      templatePath: 'template.yml',
+      s3Bucket: 'test-stacksets-us-east-1',
+      s3Prefix: '',
+      targets: {
+        [mockAWSAcctNum2]: {
+          'us-east-1': true,
+          'us-west-2': true
+        }
+      }
+    })
+  })
+  test('Returns sample cfdeploy settings object read from file when no template is set', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-noTemplatePath.yml')).resolves.toEqual({
+      stackSetName,
+      templatePath: 'template.yml',
+      s3Bucket: 'test-stacksets-us-east-1',
+      s3Prefix: '',
+      targets: {
+        [mockAWSAcctNum]: {
+          'us-east-1': true,
+          'us-west-2': true
+        }
+      }
+    })
+  })
+  test('Returns an error for a malformed config file', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-malformed.yml')).rejects.toThrow()
+  })
+  test('Returns an error when duplicate ids are present', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-duplicateIds.yml')).rejects.toThrow()
+  })
+  test('Returns an error when duplicate ids with a default are present', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-duplicateIdsDefault.yml')).rejects.toThrow()
+  })
+  test('Returns an error when stack name is missing in the config file', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-missingName.yml')).rejects.toThrow()
+  })
+  test('Returns an error when stack name is missing in the config file', () => {
+    expect.assertions(1)
+    return expect(getSettings('cfdeploy-missingS3Bucket.yml')).rejects.toThrow()
   })
 })
